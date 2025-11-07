2a8aae32da8d05d3a1f4531413246a47d8e195b1
JooseongMo0n <jsmoon4738@gmail.com>
2025-11-07 22:15:06 +0900

    개발환경세팅

--- src/main/java/com/springboot/hackathonteam1be/api/HackathonTeam1BeApplication.java
+++ src/main/java/com/springboot/hackathonteam1be/api/HackathonTeam1BeApplication.java
@@ -1,4 +1,4 @@
-package com.springboot.hackathonteam1be;
+package com.springboot.hackathonteam1be.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
