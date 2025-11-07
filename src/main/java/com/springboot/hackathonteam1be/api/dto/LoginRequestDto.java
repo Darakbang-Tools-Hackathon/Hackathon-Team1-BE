be26ba6a3758198d867afd47fefe8f27f367b8d3
JooseongMo0n <jsmoon4738@gmail.com>
2025-11-08 02:26:49 +0900

    Hi

--- src/main/java/com/springboot/hackathonteam1be/api/dto/LoginRequestDto.java
+++
@@ -1,6 +0,0 @@
-package com.springboot.hackathonteam1be.api.dto;
-
-import jakarta.validation.constraints.NotEmpty;
-import lombok.Getter;
-import lombok.Setter;
-
---
+++ src/main/java/com/springboot/hackathonteam1be/core/service/UserService.java
@@ -0,0 +1,7 @@
+package com.springboot.hackathonteam1be.core.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+}
