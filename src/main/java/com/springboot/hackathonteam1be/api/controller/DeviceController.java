3296808088453c1c2a2457a21c4e7e619850d7b3
JooseongMo0n <jsmoon4738@gmail.com>
2025-11-08 01:08:21 +0900

    Cloudinary

---
+++ src/main/java/com/springboot/hackathonteam1be/api/controller/DeviceController.java
@@ -0,0 +1,21 @@
+package com.springboot.hackathonteam1be.api.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/api/devices")
+public class DeviceController {
+//    @PostMapping("/upload/image")
+//    public ResponseEntity<String> fileUpload(@RequestParam("imageFile") MultipartFile file){
+//        if (file.isEmpty()) {
+//            return ResponseEntity.badRequest().body("업로드할 파일을 선택해 주세요.");
+//        }
+//
+//        try {
+//            String originalFilename = file.getOriginalFilename();
+//
+//        }
+//    }
+}
